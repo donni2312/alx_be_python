d13d2c1a5bea341503e446cba0e73a458e1f6d0d
donni2312 <a.donnelly2312@gmail.com>
2025-05-23 22:22:46 +0200

    add basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+addtion = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+print("Addtion of", number1, "and", number2, "is:", addtion)
+print("Subtraction of", number1, "and", number2, "is:", subtraction)
+print("Multiplication of", number1, "and", number2, "is:", multiplication)
